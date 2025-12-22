e144d8009c934644112a8e62bb91d872fafdb80b
R Pranav Yadav <153364659+RPY09@users.noreply.github.com>
2025-12-22 21:27:36 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,13 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://dhanasethu09.vercel.app",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth.routes"));
