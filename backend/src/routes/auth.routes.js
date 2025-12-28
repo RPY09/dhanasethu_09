82610bdef505deb5ca8ce36e24631f65ea9b9992
Pranav-Reddamoni <153364659+RPY09@users.noreply.github.com>
2025-12-28 19:31:44 +0530

    otps

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -10,18 +10,11 @@ router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/forgot-password-request", authController.forgotPasswordRequest);
 router.post("/login-otp", authController.loginViaOtp);
+router.post("/request-password-otp", authController.requestPasswordOtp);
 
 /* ================= PROTECTED ROUTES ================= */
 
 router.get("/refresh-token", protect, authController.refreshToken);
 router.put("/update-profile", protect, authController.updateProfile);
-router.post(
-  -"/request-password-otp",
-  -protect,
-  -authController.requestPasswordOtp + "/request-password-otp",
-  +authController.requestPasswordOtp
-);
-
-// router.post("/reset-password-otp", protect, authController.resetPasswordOtp);
 
 module.exports = router;
